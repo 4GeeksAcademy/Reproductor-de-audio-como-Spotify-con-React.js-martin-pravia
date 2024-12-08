476cca2af331c1255d86730a4c150dc0e498ab30
Martín Pravia <marto.pravia@gmail.com>
2024-12-08 12:38:26 +0000

    creoestructura del proyecto html con bootstrap

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,21 +6,21 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+		<>
+<nav className="navbar bg-body-tertiary">
+  <div className="container-fluid text-bg-success p-4">
+    <span className="navbar-brand mb-0 h1">Spotify home-made</span>
+  </div>
+  </nav>
+  <div className="text-start">
+  <ol className="list-group list-group-numbered">
+  <li className="list-group-item list-group-item-action list-group-item-success">A list item</li>
+  <li className="list-group-item list-group-item-action list-group-item-success">A list item</li>
+  <li className="list-group-item list-group-item-action list-group-item-success">A list item</li>
+</ol>
+  </div>
+  </>
+  	);
 };
 
 export default Home;
